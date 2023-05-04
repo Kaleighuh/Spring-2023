a1112e9d8eeb66b63789d3b0abb65a722613d376
Kaleighuh <10958501@uvu.edu>
2023-05-03 20:34:36 -0600

    Working through errors

--- Chortle is in Trouble/Assets/Scipts/Player.cs
+++ Chortle is in Trouble/Assets/Scipts/Player.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
     public float gravity;
     public Vector2 velocity;
+    public float maxXVelocity = 100;
     public float maxAcceleration = 10;
     public float acceleration = 10;
     public float distance = 0;
     public float jumpVelocity = 20;
-    public float maxXVelocity = 100;
     public float groundHeight = 10;
     public bool isGrounded = false;
+
     public bool isHoldingJump = false;
-    public float maxHoldJumpTime = .4f;
+    public float maxHoldJumpTime = 0.4f;
     public float holdJumpTimer = 0.0f;
+
     public float jumpGroundThreshold = 1;
+
+
     void Start()
     {
-        
+           
     }
+
     void Update()
     {
         Vector2 pos = transform.position;
         float groundDistance = Mathf.Abs(pos.y - groundHeight);
+
         if (isGrounded || groundDistance <= jumpGroundThreshold)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -40,50 +47,80 @@ public class Player : MonoBehaviour
         {
             isHoldingJump = false;
         }
+
+
+
     }
 
     private void FixedUpdate()
     {
-       // Time.fixedDeltaTime =  fixed framerate within game.
-       Vector2 pos = transform.position;
-       
-       if (!isGrounded)
-       {
-           if (isHoldingJump)
-           {
-               holdJumpTimer += Time.fixedDeltaTime;
-               if (holdJumpTimer >= maxHoldJumpTime)
-               {
-                   isHoldingJump = false;
-               }
-           }
-           pos.y += velocity.y * Time.fixedDeltaTime;
-           
-           if (!isHoldingJump)
-           {
-               velocity.y += gravity * Time.fixedDeltaTime;
-           }
+        Vector2 pos = transform.position;
 
-           if (pos.y <= groundHeight)
-           {
-               pos.y = groundHeight;
-               isGrounded = true;
-           }
-       }
+        if (!isGrounded)
+        {
+            if (isHoldingJump)
+            {
+                holdJumpTimer += Time.fixedDeltaTime;
+                if (holdJumpTimer >= maxHoldJumpTime)
+                {
+                    isHoldingJump = false;
+                }
+            }
 
-       distance += velocity.x * Time.fixedDeltaTime;
 
-       if (isGrounded)
-       {
-           float velocityRatio = velocity.x / maxXVelocity;
-           acceleration = maxAcceleration * (1 - velocityRatio);
-           velocity.x += acceleration * Time.fixedDeltaTime;
-           if (velocity.x >= maxXVelocity)
-           {
-               velocity.x = maxXVelocity;
-           }
-       }
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if (!isHoldingJump)
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
 
-       transform.position = pos;
+            Vector2 rayOrigin = new Vector2(pos.x + 0.7f, pos.y);
+            Vector2 rayDirection = Vector2.up;
+            float rayDistance = velocity.y * Time.fixedDeltaTime;
+            RaycastHit2D hit2D = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+            if (hit2D.collider != null)
+            {
+                Ground ground = hit2D.collider.GetComponent<Ground>();
+                if (ground != null)
+                {
+                    groundHeight = ground.groundHeight;
+                    pos.y = groundHeight;
+                    velocity.y = 0;
+                    isGrounded = true;
+                }
+            }
+            Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.red);
+        }
+
+        distance += velocity.x * Time.fixedDeltaTime;
+
+        if (isGrounded)
+        {
+            float velocityRatio = velocity.x / maxXVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            if (velocity.x >= maxXVelocity)
+            {
+                velocity.x = maxXVelocity;
+            }
+
+
+            Vector2 rayOrigin = new Vector2(pos.x - 0.7f, pos.y);
+            Vector2 rayDirection = Vector2.up;
+            float rayDistance = velocity.y * Time.fixedDeltaTime;
+            RaycastHit2D hit2D = Physics2D.Raycast(rayOrigin, rayDirection, rayDistance);
+            if (hit2D.collider == null)
+            {
+                isGrounded = false;
+            }
+            Debug.DrawRay(rayOrigin, rayDirection * rayDistance, Color.yellow);
+
+        }
+
+
+        transform.position = pos;
     }
+
 }
+
