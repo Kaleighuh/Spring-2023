d236bba3a71ee789a71fd39de153df3918dfa0ad
Kaleighuh <10958501@uvu.edu>
2023-05-03 20:01:47 -0600

    Parallax Background Completed

---
+++ Chortle is in Trouble/Assets/Scipts/UI_controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_controller : MonoBehaviour
+{
+    Player player;
+    Text distanceText;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        distanceText = GameObject.Find("Distance Text").GetComponent<Text>();
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    { 
+        //int distance = Mathf.FloorToInt(player.distance);
+        //distanceText.text = distance + " m";
+    }
+}
